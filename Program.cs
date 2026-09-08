11650b1797c3a1b7c310a88bd487059a481dbca8
tiftikhar141 <tiftikhar775@gmail.com>
2026-09-08 09:38:02 -0500

    started Draw method to show stages of hangman after a guess

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            
+            DisplayWord("cat");
         }
 
         static string GenerateWord()
@@ -42,20 +42,77 @@
             return words[randomIndex];
         }
 
-        static void DisplayWord(string word)
+        static void DisplayWord(string generatedWord)
         {
-            for (int i = 0; i < word.Length; i++)
+            for (int i = 0; i < generatedWord.Length; i++)
             {
                 Console.Write("_");
             }
+            Console.WriteLine();
+
+            string updatedWord = "";
+            int numberOfTries = 3;
+
+            // update word and display it after each guess
+            // if users guess has a character that matches the character in the generated word in the same location then show that character
+            // otherwise continue to show _
+            // ie) generatedWord = cat, guess = car, updatedWord should be ca_
+            string guess;
+            do
+            {
+                guess = Console.ReadLine();
+
+                // condition in case word is too large (out of bounds array)
+                if (guess.Length > guess.Length)
+                {
+                    
+                }
+
+                for (int i = 0; i < guess.Length; i++)
+                {
+                    // if character in userinput matches character in generated word
+                    if (guess[i] == generatedWord[i])
+                    {
+                        updatedWord += guess[i];
+                    } else
+                    {
+                        updatedWord += "_";
+                    }
+                }
+                if (guess == generatedWord)
+                {
+                    Console.WriteLine("\nCongratulations you got the word!");
+                    break;
+                }
+                numberOfTries--;
+                Console.WriteLine($"{updatedWord}");
+                updatedWord = "";
+                
+                Draw(numberOfTries);
+
+            } while (numberOfTries > 0);
+
+            Console.WriteLine("Game Over!");
         }
 
-        static void Draw()
+        static void Draw(int stage)
         {
             // Final Sprite should look like this
-            Console.WriteLine(" O");
-            Console.WriteLine("-|-");
-            Console.WriteLine(@" /\");
+            if (stage == 2)
+            {
+                Console.WriteLine("O");
+            }
+            if (stage == 1)
+            {
+                Console.WriteLine("O");
+                Console.WriteLine("|");
+            }
+            if (stage == 0)
+            {
+                Console.WriteLine("O");
+                Console.WriteLine("|");
+                Console.WriteLine(@"/\");
+            }
         }
     }
 }
