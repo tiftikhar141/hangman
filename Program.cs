5ea5bef9990422bba9cd45be278a1dc5a102a065
tiftikhar141 <tiftikhar775@gmail.com>
2026-09-07 19:35:37 -0500

    added method to generate random word from list of words

--- Program.cs
+++ Program.cs
@@ -4,7 +4,51 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string word = GenerateWord();
+            Console.WriteLine(word);
+        }
+
+        static string GenerateWord()
+        {
+            List<string> words = new List<string>
+            {
+                "Biblioklept", 
+                "Nauseant", 
+                "Addend", 
+                "Agelast", 
+                "Peristeronic",
+                "Hibernal",
+                "Subnivean",
+                "Sitzmark",
+                "Primaveral",
+                "Solivagant",
+                "Filipendulous",
+                "Jentacular",
+                "Catillate",
+                "Bellycheer",
+                "Avidulous",
+                "Retrogradation",
+                "Spinous",
+                "Illaudable",
+                "Grimoire",
+                "Fantod",
+                "Dyspathy",
+                "Orgulous",
+                "Dilapidator",
+                "Crimpy",
+                "Jubilarian"
+            };
+            int randomIndex = Random.Shared.Next(0, words.Count);
+
+            return words[randomIndex];
+        }
+
+        static void Draw()
+        {
+            // Final Sprite should look like this
+            Console.WriteLine(" O");
+            Console.WriteLine("-|-");
+            Console.WriteLine(@" /\");
         }
     }
 }
