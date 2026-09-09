2d572a771a285ac9d6b34002cc76b5c218602eae
tiftikhar141 <tiftikhar775@gmail.com>
2026-09-09 15:17:26 -0500

    initial commit

--- obj/Debug/net10.0/Hangman.AssemblyInfo.cs
+++ obj/Debug/net10.0/Hangman.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hangman")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a641d6714faae052e40831dbede62f0c2fbd36bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hangman")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hangman")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
